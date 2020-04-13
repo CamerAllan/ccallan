34641b43233766ad391861277fa010ee4de11a70
Cameron Allan <cameronallandev@gmail.com>
2020-04-13 23:02:28 +0100

    Add support and formatting for images in markdown

--- src/containers/Post.tsx
+++ src/containers/Post.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import { useRouteData } from 'react-static'
 import ReactMarkdown from 'react-markdown'
 import ColorLink from 'components/util/Link'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+  image: {
+    display: 'block',
+    margin: '0 auto',
+    maxWidth: '95%'
+  }
+}))
 
 export default () => {
   const { post }: { post: any } = useRouteData()
+  const classes = useStyles();
+
+  const imageRenderer = (props: any) => {
+    return <img className={classes.image} {...props} />
+  }
+
   return (
     <>
       <div>
@@ -12,7 +27,10 @@ export default () => {
         <h1>
           {post.data.title}
         </h1>
-        <ReactMarkdown source={post.content}/>
+        <ReactMarkdown 
+          transformImageUri={(imageName) => `/images/posts/${post.data.id}/${imageName}`} 
+          source={post.content} 
+          renderers={{image: imageRenderer}}/>
       </div>
     </>
   )
