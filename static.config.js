2a53249b173d1f07256a5c6ca6ebf78030de4677
Cameron Allan <cameronallandev@gmail.com>
2020-04-13 22:04:50 +0100

    Add code for displaying favicon

--- static.config.js
+++ static.config.js
@@ -9,12 +9,12 @@ const generateClassName = createGenerateClassName()
 const blogPath = 'public/content/posts/';
 
 const wordCount = (content) => {
-    return (content.split(" ").length - 1)
+  return (content.split(" ").length - 1)
 }
 
 const getPosts = () => {
   const posts = []
-  
+
   fs.readdirSync(blogPath).forEach(file => {
     // Get contents of markdown file
     const contents = fs.readFileSync(blogPath + file, 'utf8')
@@ -64,22 +64,22 @@ export default {
             <title>Cameron Allan</title>
 
             {/* Favicon business */}
-            <link rel="apple-touch-icon" sizes="57x57" href="/public/favicon/apple-icon-57x57.png"/>
-            <link rel="apple-touch-icon" sizes="60x60" href="/public/favicon/apple-icon-60x60.png"/>
-            <link rel="apple-touch-icon" sizes="72x72" href="/public/favicon/apple-icon-72x72.png"/>
-            <link rel="apple-touch-icon" sizes="76x76" href="/public/favicon/apple-icon-76x76.png"/>
-            <link rel="apple-touch-icon" sizes="114x114" href="/public/favicon/apple-icon-114x114.png"/>
-            <link rel="apple-touch-icon" sizes="120x120" href="/public/favicon/apple-icon-120x120.png"/>
-            <link rel="apple-touch-icon" sizes="144x144" href="/public/favicon/apple-icon-144x144.png"/>
-            <link rel="apple-touch-icon" sizes="152x152" href="/public/favicon/apple-icon-152x152.png"/>
-            <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-icon-180x180.png"/>
-            <link rel="icon" type="image/png" sizes="192x192"  href="/public/favicon/android-icon-192x192.png"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon/favicon-32x32.png"/>
-            <link rel="icon" type="image/png" sizes="96x96" href="/public/favicon/favicon-96x96.png"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
-            <link rel="manifest" href="/manifest.json"/>
-            <meta name="msapplication-TileColor" content="#ffffff"/>
-            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+            <link rel="apple-touch-icon" sizes="57x57" href="/public/favicon/apple-icon-57x57.png" />
+            <link rel="apple-touch-icon" sizes="60x60" href="/public/favicon/apple-icon-60x60.png" />
+            <link rel="apple-touch-icon" sizes="72x72" href="/public/favicon/apple-icon-72x72.png" />
+            <link rel="apple-touch-icon" sizes="76x76" href="/public/favicon/apple-icon-76x76.png" />
+            <link rel="apple-touch-icon" sizes="114x114" href="/public/favicon/apple-icon-114x114.png" />
+            <link rel="apple-touch-icon" sizes="120x120" href="/public/favicon/apple-icon-120x120.png" />
+            <link rel="apple-touch-icon" sizes="144x144" href="/public/favicon/apple-icon-144x144.png" />
+            <link rel="apple-touch-icon" sizes="152x152" href="/public/favicon/apple-icon-152x152.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-icon-180x180.png" />
+            <link rel="icon" type="image/png" sizes="192x192" href="/public/favicon/android-icon-192x192.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="96x96" href="/public/favicon/favicon-96x96.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
 
             <meta name="theme-color" content='#031926' />
           </Head>
