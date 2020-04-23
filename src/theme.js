264f39937427c10913b4db743551183d0e697992
Cameron Allan <cameronallandev@gmail.com>
2020-04-23 23:33:05 +0100

    Swapped colours

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ const dark = '#25323A'
 const medium = '#89AFA9'
 const light = '#CFD2CD'
 
-export const theme = createMuiTheme({
+export const lightTheme = createMuiTheme({
   palette: {
     type: 'light',
     text: {
@@ -45,4 +45,44 @@ export const theme = createMuiTheme({
   }
 });
 
-export default theme;
+export const darkTheme = createMuiTheme({
+  palette: {
+    type: 'light',
+    text: {
+      primary: light,
+      secondary: '#ffffff'
+    },
+    primary: {
+      main: medium,
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: light,
+    },
+    info: {
+      main: dark
+    },
+    background: {
+      default: dark
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 650,
+      md: 960,
+      lg: 1200,
+      xl: 1920
+    }
+  }
+});
+
+export default lightTheme;
