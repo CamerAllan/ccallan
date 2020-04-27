036a0d63292bdc2f4ee3b5794bb5d70d92358410
Cameron Allan <cameronallandev@gmail.com>
2020-04-27 21:34:38 +0100

    Add image placeholders

--- src/components/ItemContent.tsx
+++ src/components/ItemContent.tsx
@@ -5,12 +5,17 @@ import { makeStyles, Theme, Box } from '@material-ui/core'
 import { SPACING, IMAGE_DIMENSIONS } from '../constants/Constants'
 import { ColorLinkExternal } from './util/Link';
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = (preview: boolean) => makeStyles((theme: Theme) => ({
     image: {
         display: 'block',
         margin: '0 auto',
         maxWidth: '95%',
         border: `0.4rem solid ${theme.palette.primary.main}`
+    },
+    imagePlaceholder: {
+        width: preview ? IMAGE_DIMENSIONS.PREVIEW.WIDTH : IMAGE_DIMENSIONS.FULL.WIDTH,
+        height: preview ? IMAGE_DIMENSIONS.PREVIEW.HEIGHT : IMAGE_DIMENSIONS.FULL.HEIGHT,
+        backgroundColor: 'primary'
     },
     para: {
         margin: theme.spacing(SPACING.SMALL)
@@ -34,14 +39,18 @@ export default (props: ItemContentProps) => {
 
     const { item: { id, content, description }, containingFolder, preview, onlyDescription } = props
 
-    const classes = useStyles();
+    const classes = useStyles(preview)();
 
     const linkRenderer = (props: any) => {
         return <ColorLinkExternal to={props.href} {...props} />
     }
 
     const imageRenderer = (props: any) => {
-        return <img className={classes.image} {...props} />
+        return (
+            <React.Suspense fallback={<Box className={classes.imagePlaceholder}></Box>}>
+                <img className={classes.image} {...props} />
+            </React.Suspense>
+        )
     }
 
     const paragraphRenderer = (props: any) => {
