33f8db3de144d17576881825af4f6936901df8df
Cameron Allan <cameronallandev@gmail.com>
2020-09-13 19:02:23 +0100

    404, Add post: Don't stop now

---
+++ public/content/music/dont-start-now.md
@@ -0,0 +1,9 @@
+---
+date: 9/12/20
+title: Don't Start Now | Pomplamoose
+# description: 
+long: false
+live: true
+---
+
+[Very snazzy bassline](https://www.youtube.com/watch?v=iJ65wvqcK1M)
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,9 @@
+import { ColorLinkInternal } from 'components/util/Link'
 import React from 'react'
 
 export default () => (
   <div>
-    <h1>There's nothing here just yet, still working on it.</h1>
+    <h1>Looks like there's nothing here!</h1>
+    <body>Head back <ColorLinkInternal to="/">Home</ColorLinkInternal></body>
   </div>
 )
