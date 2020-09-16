1b93d344b82bb827f3e67af9a84ff4d63916df75
Cameron Allan <cameronallandev@gmail.com>
2020-09-16 17:10:23 +0100

    Bro get you job out of there

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,6 @@ export default () => {
           Home
         </h1>
         <p> Hi, I'm <b>Cameron Allan</b>.</p>
-        <p> I'm currently software engineering at <ColorLinkExternal to='https://www.dunnhumby.com'>dunnhumby</ColorLinkExternal> </p>
         <p> I built this site to encourage myself to write more. There's a <ColorLinkInternal to='/bits-and-bobs/posts'>blog</ColorLinkInternal> featuring fully formed thoughts and coherent sentences, then there's <ColorLinkInternal to='/bits-and-bobs'>bits & bobs</ColorLinkInternal> featuring everything else.</p>
         <p> This gives me a space to start typing no matter what level of effort I feel like putting in, so no excuses. </p>
         <p> Feel free to reach out to me for any reason at <ColorLinkExternal to='mailto:cameronallandev@gmail.com'>cameronallandev@gmail.com</ColorLinkExternal></p>
